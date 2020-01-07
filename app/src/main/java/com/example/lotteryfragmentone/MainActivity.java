9c9ba9a092be7304848e2e928853e7a2ece0e69b
rainer <rainer.telesko@fhnw.ch>
2020-01-07 09:14:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lotteryfragmentone/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lotteryfragmentone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // Project Source: https://www.javatpoint.com/android-fragments
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
