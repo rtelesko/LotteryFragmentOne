904a3db340c0776399c68f55c23c7b6904cbac82
rainer <rainer.telesko@fhnw.ch>
2020-02-29 16:25:57 +0100

    Third commit

--- app/src/main/java/com/example/lotteryfragmentone/Fragment1.java
+++ app/src/main/java/com/example/lotteryfragmentone/Fragment1.java
@@ -20,13 +20,20 @@ public class Fragment1 extends Fragment {
     A LayoutInflater reads an XML in which we describe how we want a UI layout to be.
     It then creates actual View objects for UI from that XML.
     container: ViewGroup: If non-null, this is the parent view that the fragment's UI should be attached to.
-    ViewGroup: If non-null, this is the parent view that the fragment's UI should be attached to.
     savedInstanceState: Bundle: If non-null, this fragment is being re-constructed from a previous saved state as given here.
-     */
+    */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        /*
+        The inflate() method takes three parameters:
+        The id of a layout XML file
+        a parent ViewGroup into which the fragment's View is to be inserted,
+        and a third boolean:
+            true: add the child view to parent RIGHT NOW
+            false: add the child view to parent NOT NOW
+        */
         return inflater.inflate(R.layout.fragment1, container, false);
     }
 }
